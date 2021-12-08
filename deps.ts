d7857a3752b4ce0cabada0784d7083eca5218e55
Ryan Aunur <ryandevstudio@gmail.com>
2021-12-08 13:42:03 +0700

    refactor: replace opine to std serve

--- deps.ts
+++ deps.ts
@@ -14,6 +14,6 @@ export { Url } from "https://cdn.skypack.dev/reurl";
 
 export { DOMParser, Element } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
 
-export { opine, json } from "https://deno.land/x/opine@2.0.0/mod.ts";
-
 export { assertEquals } from "https://deno.land/std@0.117.0/testing/asserts.ts";
+
+export { serve } from "https://deno.land/std@0.117.0/http/server.ts"
