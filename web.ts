a40edc2fc83e20b10e664842d7058a0b376d5a56
Ryan Aunur <ryandevstudio@gmail.com>
2021-12-16 22:40:36 +0700

    update: remove graphiql, add endpoint args

--- web.ts
+++ web.ts
@@ -1,34 +1,4 @@
-export const graphiqlHTML = (port: string) => {
-  return `<html>
-  <head>
-    <title>Simple GraphiQL Example</title>
-    <link href="https://unpkg.com/graphiql/graphiql.min.css" rel="stylesheet" />
-  </head>
-  <body style="margin: 0;">
-    <div id="graphiql" style="height: 100vh;"></div>
-    <script crossorigin src="https://unpkg.com/react/umd/react.production.min.js"></script>
-    <script crossorigin src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"></script>
-    <script crossorigin src="https://unpkg.com/graphiql/graphiql.min.js"></script>
-    <script>
-      const graphQLFetcher = graphQLParams =>
-        fetch('http://localhost:${port}/graphql', {
-          method: 'post',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(graphQLParams),
-        })
-          .then(response => response.json())
-          .catch(() => response.text());
-      ReactDOM.render(
-        React.createElement(GraphiQL, { fetcher: graphQLFetcher }),
-        document.getElementById('graphiql'),
-      );
-    </script>
-  </body>
-  </html>
-  `
-}
-
-export const prismqlPlayground = (port: string | number) => {
+export const prismqlPlayground = (port: string | number, endpoint: string) => {
   return `
   <!DOCTYPE html>
 <html>
@@ -82,7 +52,7 @@ export const prismqlPlayground = (port: string | number) => {
   </div>
   <script>window.addEventListener('load', function (event) {
       GraphQLPlayground.init(document.getElementById('root'), {
-        endpoint: 'http://localhost:${port}/graphql'
+        endpoint: '${endpoint}'
       })
     })</script>
 </body>
